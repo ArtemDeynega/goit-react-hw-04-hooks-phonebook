df77fd1e05f431c2e6d7f1cf3a0a61a8256ee7d4
artem <73790146+ArtemDeynega@users.noreply.github.com>
2022-05-11 06:59:26 +0300

    ContactEditor done

--- src/components/ContactEditor/ContactEditor.jsx
+++ src/components/ContactEditor/ContactEditor.jsx
@@ -1,57 +1,56 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Form, Label, Input, Button } from '.';
 
-export class ContactEditor extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  handleChange = evt => {
-    const { name, value } = evt.target;
-    this.setState({ [name]: value.trim() });
+const initialContact = {
+  name: '',
+  number: '',
+};
+export const ContactEditor = ({ onSubmit }) => {
+  const [contact, setContact] = useState(initialContact);
+
+  const handleChange = evt => {
+    setContact({ ...contact, [evt.target.name]: evt.target.value.trim() });
   };
 
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
 
-    this.props.onSubmit(this.state);
-    this.setState({ name: '', number: '' });
+    onSubmit(contact);
+    setContact(initialContact);
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <>
-        <Form autoComplete="off" onSubmit={this.handleSubmit}>
-          <Label htmlFor={name}>
-            Name
-            <Input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              placeholder="Enter name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </Label>
-          <Label htmlFor={number}>
-            Phone
-            <Input
-              type="tel"
-              name="number"
-              value={number}
-              placeholder="Enter phone"
-              onChange={this.handleChange}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </Label>
-          <Button type="submit">Add contact</Button>
-        </Form>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Form autoComplete="off" onSubmit={handleSubmit}>
+        <Label htmlFor={contact.name}>
+          Name
+          <Input
+            type="text"
+            name="name"
+            value={contact.name}
+            onChange={handleChange}
+            placeholder="Enter name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+        <Label htmlFor={contact.number}>
+          Phone
+          <Input
+            type="tel"
+            name="number"
+            value={contact.number}
+            placeholder="Enter phone"
+            onChange={handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </>
+  );
+};
+export default ContactEditor;
